3bcb73de7060e27f86bd6d739f26abaa5c34ea7f
RAMPRAVIN <52540716+RAMPRAVIN@users.noreply.github.com>
2019-11-06 21:22:08 +0530

    Add files via upload

---
+++ JSONmerge.py
@@ -0,0 +1,23 @@
+import json
+f_p=input("Enter the folder path :")
+fn1=input("Enter the input file name 1 :")
+fn2=input("Enter the input file name 2 :")
+fn3=input("Enter the input file name 3 :")
+fn4=input("Enter the output file name :")
+fs=int(input("Enter the maximum file size (in bytes) :"))
+#content
+f1=fn1+'.json'
+f2=fn2+'.json'
+f3=fn3+'.json'
+f4=fn4+'.json'
+with open (f_p/f1) as f:
+    d1=json.load(f)
+with open (f_p/f2) as g:
+    d2=json.load(g)
+with open (f_p/f3) as h:
+    d3=json.load(h)
+val=[d1,d2,d3]
+
+#writing
+with open(f4,'w') as jsonfile:
+    json.dump(val,jsonfile)
